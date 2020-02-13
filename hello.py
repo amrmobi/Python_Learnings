ccff7191544bfc018214d32a22f6efdc00f4b97c
amrmobi <amr.mobi@gmail.com>
2020-02-12 19:48:26 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,23 @@
-#Hello
-print('Hello')
+#This is single line comment!
+"""
+This
+is
+a
+Multiple line comment!!!
+"""
+print ("My first line in Python programming!")
+
+#Do a condition
+
+if (2>1):
+    print('My first condition in python!')
+
+"""
+Define, Assign Variable!
+Spit it out!!!
+"""
+a = "Awesome"
+print("Print variable a " + a);
+
+b = 10
+print('Print variable b '  + str(b));
